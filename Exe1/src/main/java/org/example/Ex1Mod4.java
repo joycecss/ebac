90d09ef1585e52b0fa89307b0763e05b8aba0ceb
joyce.silva <joyce.silva@valemobi.com.br>
2023-07-07 11:26:25 -0300

    Exercicio 1 - Modulo 4

--- Exe1/src/main/java/org/example/Ex1Mod4.java
+++ Exe1/src/main/java/org/example/Ex1Mod4.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Ex1Mod4 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world");
     }
 }
