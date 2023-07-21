79badab11c0359335ea2ebde92dbe972b76f6e94
joyce.silva <joyce.silva@valemobi.com.br>
2023-07-21 11:55:36 -0300

    Feat: Ex do mod13

---
+++ mod13/ExMod13/src/main/java/org/example/Pessoa.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public abstract class Pessoa {
+    private String nome;
+    private String genero;
+    private int idade;
+    private String nacionalidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+}
