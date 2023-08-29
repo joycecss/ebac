17bb9865564b219b9ba58504688c91dfefa6bf4d
joyce.silva <joyce.silva@valemobi.com.br>
2023-08-29 11:24:48 -0300

    Feat: Exe mod22

---
+++ tarefa_streams/Streams/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Digite nome e o sexo usando a travessão para separar e virgula para adicionar uma nova pessoa(Exemplo: alex-m,carla-f)");
+        Scanner scanner = new Scanner(System.in);
+        String peoples = scanner.nextLine();
+
+        String[] separatePeoples = peoples.split(",");
+
+        Stream<String> womenStream = Arrays.stream(separatePeoples)
+                .filter(people -> {
+                       var peopleFormatted = people.toLowerCase().toCharArray();
+                       return peopleFormatted[peopleFormatted.length-1] == 'f';
+                });
+
+        womenStream.forEach(System.out::println);
+    }
+}
